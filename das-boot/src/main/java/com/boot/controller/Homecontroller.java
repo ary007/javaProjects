0b83e8436c8062d87ef5bfc83363f68df51f74c2
ary007 <sarkararijeet7@gmail.com>
2017-04-17 23:59:54 +0530

    Integrate UI and flyway

--- das-boot/src/main/java/com/boot/controller/Homecontroller.java
+++ das-boot/src/main/java/com/boot/controller/Homecontroller.java
@@ -1,14 +1,19 @@
 package com.boot.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.model.Shipwreck;
 
 @RestController
 public class Homecontroller {
 
-	@RequestMapping("/")
+	@RequestMapping(value="/")
 	public String home() {
-		return "Das-boot";
+		return "hi";
 	}
 
 }
